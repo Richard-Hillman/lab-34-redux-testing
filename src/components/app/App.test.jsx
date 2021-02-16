d1688eeca08f84aaa53143c9245cb9730ae5a9c7
Richard Hillman <Richard.CC.Hillman@gmail.com>
2021-02-16 14:46:41 -0800

    all three tests created and passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -51,8 +51,34 @@ describe('color picker change and record undo and redo ', () => {
     fireEvent.click(colorUndo);
     
     return waitFor(() => {
-      expect(colorPicker).toHaveValue('#ff0000');
+      // expect(colorPicker).toHaveValue('#ff0000');
       expect(colorDisplay.style.backgroundColor).toBe('rgb(255, 0, 0)');
+    });
+
+  });
+
+  // ------------------------------------------------
+
+  it('redoes the color ', async() => {
+    render(<App />);
+
+    const colorPicker = await screen.findByLabelText('Current Color');
+    const colorDisplay = await screen.findByTestId('frig');
+    const colorUndo = await screen.findByText('undo');
+    const colorRedo = await screen.findByText('redo');
+
+    fireEvent.change(colorPicker, {
+      target:{
+        value: '#00FF00'
+      }
+    });
+
+    fireEvent.click(colorUndo);
+    fireEvent.click(colorRedo);
+
+    return waitFor(() => {
+      // expect(colorPicker).toHaveValue('#ff0000');
+      expect(colorDisplay.style.backgroundColor).toBe('rgb(0, 255, 0)');
     });
 
   });
